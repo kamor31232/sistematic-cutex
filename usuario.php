6cbfbffc974c14bcd4e1fad4157b1207cc960e12
Katerine Morcillo <katerine.morcillo@misena.edu.co>
2022-12-07 18:44:30 -0500

    paginacion usuarios

--- usuario.php
+++ usuario.php
@@ -1,6 +1,19 @@
 <?php include("./includes/header.php") ?>
 <?php include("./database/db.php") ?>
 
+<?php
+$limit = 10;
+$offset = 0;
+$filter = '';
+
+if (isset($_GET['search'])) {
+  $filter = $_GET['search'];
+}
+if (isset($_GET['pages'])) {
+  $offset = $_GET['pages'];
+}
+?>
+
 <div class="row">
   <?php include("./includes/aside.php") ?>
   <section class="col-9 section-general">
@@ -23,10 +36,11 @@
       <?php session_unset();
       } ?>
       <div class="input-group col-6 mb-4">
-        <input type="text" class="form-control" placeholder="Nombre ó documento" aria-label="Text input with dropdown button">
-        <div class="input-group-append">
-          <button class="btn btn-outline-secondary" type="button">Buscar</button>
-        </div>
+        <!-- Buscador -->
+        <form>
+          <input type="text" class="form-control" name="search" value="<?php echo $filter ?>" placeholder="Nombre ó documento" aria-label="Text input with dropdown button">
+        </form>
+
       </div>
       <table class="table">
         <thead>
@@ -42,8 +56,11 @@
         </thead>
         <tbody>
           <?php
+
+          $offsetPage = $limit * $offset;
           $query = "SELECT u.id as id, u.names as names, u.surnames as surnames, u.document_number as document, u.email as email, r.name as rol, u.status as status
-          FROM user as u INNER JOIN rol as r ON u.rol_id= r.id";
+          FROM user as u INNER JOIN rol as r ON u.rol_id= r.id WHERE u.names LIKE '%$filter%' OR u.document_number LIKE '%$filter%' LIMIT $limit OFFSET $offsetPage";
+
           $result_user = mysqli_query($conn, $query);
 
 
@@ -77,13 +94,37 @@
         </tbody>
       </table>
       <ul class="pagination justify-content-center">
-        <li class="page-item disabled">
-          <a class="page-link">Anterior</a>
-        </li>
-        <li class="page-item"><a class="page-link" href="#">1</a></li>
-        <li class="page-item">
-          <a class="page-link" href="#">Siguiente</a>
-        </li>
+        <?php
+
+        $query = "SELECT COUNT(*) as ctn
+        FROM user as u INNER JOIN rol as r ON u.rol_id= r.id WHERE u.names LIKE '%$filter%' OR u.document_number LIKE '%$filter%'";
+
+        $pages = mysqli_query($conn, $query);
+
+        if (mysqli_num_rows($pages) === 1) {
+
+          $row = mysqli_fetch_array($pages);
+          $totalCount = $row['ctn'];
+          $totalPages = ceil($totalCount / $limit);
+
+        ?>
+          <?php if ($offset > 0) { ?>
+            <li class="page-item">
+              <a href="usuario.php?search=<?php echo $filter ?>&pages=<?php echo $offset  - 1 ?>" class="page-link">Anterior</a>
+            </li>
+          <?php }  ?>
+
+          <?php
+          for ($i = 1; $i <= $totalPages; $i++) { ?>
+            <li class="page-item"><a class="page-link" href="#"><?php echo $i ?></a></li>
+          <?php } ?>
+          <?php if (($offset + 1) < $totalPages) { ?>
+            <li class="page-item">
+              <a class="page-link" href="usuario.php?search=<?php echo $filter ?>&pages=<?php echo $offset  + 1 ?>">Siguiente</a>
+            </li>
+          <?php } ?>
+
+        <?php } ?>
       </ul>
 
     </div>
